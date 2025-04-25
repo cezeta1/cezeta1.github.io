5b2c6495a06d4d0c5c198cc5a1cd9dad6eb66e8b
Cezeta <julic206@gmail.com>
2025-04-25 18:17:27 +1000

    updated grecaptcha

--- src/app/core/components/grecaptcha-badge/grecaptcha-badge.component.ts
+++ src/app/core/components/grecaptcha-badge/grecaptcha-badge.component.ts
@@ -1,7 +1,3 @@
-// .grecaptcha-badge { 
-//   visibility: hidden; 
-//   }
-
 import { Component } from "@angular/core";
 
 @Component({
@@ -10,17 +6,12 @@ import { Component } from "@angular/core";
   template: `
     <span class="text-xs text-muted-color [&>_a]:text-primary-color">
       This site is protected by reCAPTCHA and the Google
-      <a (click)="goToPrivacy()">
-        Privacy Policy
-      </a> 
+      <a (click)="goToPrivacy()"> Privacy Policy </a> 
       and
-      <a (click)="goToTerms()">
-        Terms of Service
-      </a> 
+      <a (click)="goToTerms()"> Terms of Service </a> 
       apply.
     </span>
-  `,
-  styleUrl: 'grecaptcha-badge.component.scss'
+  `
 })
 export class GRecaptchaBadgeComponent {
   protected goToPrivacy = () => window.open("https://policies.google.com/privacy", '_blank');
